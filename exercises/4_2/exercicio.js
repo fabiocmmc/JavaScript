cd5344e660ca77acc27f6748efa7840876a1992e
Fabio Camargos <fabiocmmcm@gmail.com>
2020-07-14 19:05:44 -0300

    exercicios Aula2 Bloco 4

--- exercises/4_2/exercicio.js
+++ exercises/4_2/exercicio.js
@@ -1,5 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+
 console.log(numbers);
 
 for(let i=0; i < numbers.length; i++){
@@ -11,9 +12,33 @@ for(let i=0; i < numbers.length; i++){
             console.log("Valores menores que 20: " +soma);
 
         }
-}
+}   
+    //Exercicios 2,3 e 4
+
     console.log("\n");
     console.log(soma);
     console.log(soma/numbers.length)
+   
+    //Exercicios 5 e 7
 
+for (let i=0; i < numbers.length; i++){
+    for(let j=i; j<numbers.length; j++){
+        if(numbers[i] > numbers[j]){
+            aux = numbers[i]
+            numbers[i] = numbers[j]
+            numbers[j] = aux
 
+        }
+    }
+}
+    console.log(numbers)
+    console.log("\n")
+
+for (let i=0; i < numbers.length; i++){
+    if (numbers[i] % 2 ==0){
+        console.log ("Numero Par")
+
+    }else{
+        console.log("Numero Impar")
+    }
+}
